4c786d213d9d7149d2ead3bb9e079cced94c5761
Trinaldi Rizki Permana <trinaldirizki@gmail.com>
2017-12-01 16:23:33 +0300

    Initial commit

---
+++ app/src/main/java/com/trinaldi/opencvtutorial/MainActivity.java
@@ -0,0 +1,18 @@
+package com.trinaldi.opencvtutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Mat mat;
+        mat = new Mat();
+    }
+}
